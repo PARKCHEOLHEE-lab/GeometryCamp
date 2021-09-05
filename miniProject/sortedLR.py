b7bf00f0e51109f2f467902a5a1fd1de50d7d0da
PCH <qhqhahah@naver.com>
2021-09-05 23:33:09 +0900

    Hard Coding Test

---
+++ miniProject/sortedLR.py
@@ -0,0 +1,11 @@
+﻿import Rhino.Geometry as rg
+import rhinoscriptsyntax as rs
+import ghpythonlib.components as gh
+
+vertices = rs.CurvePoints(boundary)
+centroid = rs.CurveAreaCentroid(boundary)[0]
+
+pt1 = rs.AddPoint(vertices[0][0],vertices[1][1], 0)
+pt2 = rs.AddPoint(vertices[4][0], vertices[7][1], 0)
+
+bounding = rg.Rectangle3d(rg.Plane.WorldXY, rs.PointCoordinates(pt1), rs.PointCoordinates(pt2))
