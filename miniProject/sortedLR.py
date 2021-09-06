aa9e2a3ba1d4253e7cfecf66ce61830830f31c1a
PCH <qhqhahah@naver.com>
2021-09-06 16:26:34 +0900

    Bounding Box Test

--- miniProject/sortedLR.py
+++ miniProject/sortedLR.py
@@ -1,11 +1,36 @@
 ﻿import Rhino.Geometry as rg
 import rhinoscriptsyntax as rs
-import ghpythonlib.components as gh
 
-vertices = rs.CurvePoints(boundary)
-centroid = rs.CurveAreaCentroid(boundary)[0]
+class BoundingBox:
+    def __init__(self, polygon):
+        vertices = rs.CurvePoints(polygon)
+        self.vertices = vertices
+        
+    def base(self):
+        x_list = []
+        y_list = []
+        for point in self.vertices:
+            x_pt = point[0]
+            y_pt = point[1]
+            x_list.append(x_pt)
+            y_list.append(y_pt)
+        
+        x_list = [min(x_list), max(x_list)]
+        y_list = [min(y_list), max(y_list)]
+        
+        self.base_pt_1 = rs.AddPoint(min(x_list), min(y_list))
+        self.base_pt_2 = rs.AddPoint(max(x_list), max(y_list))
+        self.base_pt_1 = rs.PointCoordinates(self.base_pt_1)
+        self.base_pt_2 = rs.PointCoordinates(self.base_pt_2)
+        
+        return self.base_pt_1, self.base_pt_2
+        
+    def box(self):
+        plane = rg.Plane.WorldXY
+        bounding_box = rg.Rectangle3d(plane, self.base()[0], self.base()[1])
+        
+        return bounding_box
 
-pt1 = rs.AddPoint(vertices[0][0],vertices[1][1], 0)
-pt2 = rs.AddPoint(vertices[4][0], vertices[7][1], 0)
 
-bounding = rg.Rectangle3d(rg.Plane.WorldXY, rs.PointCoordinates(pt1), rs.PointCoordinates(pt2))
+POLYGON = BoundingBox(polygon)
+bounding_box = POLYGON.box()
