7b4386d3b05c461e6718f9bacc5d6787323ddd4b
PCH <qhqhahah@naver.com>
2021-09-09 14:07:20 +0900

    Geometry Fundamentals : Two Points 3d Matrix

--- geometryFundamental/TwoPointsMatrix/TwoPointsMatrix.py
+++ geometryFundamental/TwoPointsMatrix/TwoPointsMatrix.py
@@ -39,52 +39,73 @@ class Matrix(Point):
         return points_dict
         
     def distance(self):
-        p1_x = self.points()['p1']['x']; p1_y = self.points()['p1']['y']
-        p2_x = self.points()['p2']['x']; p2_y = self.points()['p2']['y']
+        p1_x = self.points()['p1']['x']; p1_y = self.points()['p1']['y']; p1_z = self.points()['p1']['z']
+        p2_x = self.points()['p2']['x']; p2_y = self.points()['p2']['y']; p2_z = self.points()['p2']['z']
         x_distance = abs(p1_x - p2_x)
         y_distance = abs(p1_y - p2_y)
-        distance_dict = {'x':x_distance, 'y':y_distance}
+        z_distance = abs(p1_z - p2_z)
+        distance_dict = {'x':x_distance, 'y':y_distance, 'z':z_distance}
         
         return distance_dict
         
     def interval(self):
         x_interval = self.distance()['x'] / self._cnt
         y_interval = self.distance()['y'] / self._cnt
-        interval_dict = {'x':x_interval, 'y':y_interval}
+        z_interval = self.distance()['z'] / self._cnt
+        interval_dict = {'x':x_interval, 'y':y_interval, 'z':z_interval}
         
         return interval_dict
         
     def grid(self):
-        def grid_gen(pos_1, pos_2):
+        def grid_gen(pos_1, pos_2, pos_3):
             for i in range(self._cnt+1):
                 for j in range(self._cnt+1):
-                    x = i*self.interval()['x']+pos_1
-                    y = j*self.interval()['y']+pos_2
-                    pt = rs.AddPoint(x,y,0)
-                    self._pts.append(pt)
+                    for k in range(self._cnt+1):
+                        x = i*self.interval()['x']+pos_1
+                        y = j*self.interval()['y']+pos_2
+                        z = k*self.interval()['z']+pos_3
+                        pt = Point(x,y,z).generate()
+                        self._pts.append(pt)
                     
-        p1_x = self.points()['p1']['x']; p1_y = self.points()['p1']['y']
-        p2_x = self.points()['p2']['x']; p2_y = self.points()['p2']['y']
+        p1_x = self.points()['p1']['x']; p1_y = self.points()['p1']['y']; p1_z = self.points()['p1']['z']
+        p2_x = self.points()['p2']['x']; p2_y = self.points()['p2']['y']; p2_z = self.points()['p2']['z']
         
         if p1_x < p2_x:
             if p1_y < p2_y:
-                grid_gen(p1_x, p1_y)
+                if p1_z <= p2_z:
+                    grid_gen(p1_x, p1_y, p1_z)
+                
+                elif p1_z > p2_z:
+                    grid_gen(p1_x, p1_y, p2_z)
                 
             elif p1_y > p2_y:
-                grid_gen(p1_x, p2_y)
+                if p1_z <= p2_z:
+                    grid_gen(p1_x, p2_y, p1_z)
+                
+                elif p1_z > p2_z:
+                    grid_gen(p1_x, p2_y, p2_z)
                     
         if p1_x > p2_x:
             if p1_y > p2_y:
-                grid_gen(p2_x, p2_y)
+                if p1_z > p2_z:
+                    grid_gen(p2_x, p2_y, p2_z)
                 
+                elif p1_z <= p2_z:
+                    grid_gen(p2_x, p2_y, p1_z)
+                    
             elif p1_y < p2_y:
-                grid_gen(p2_x, p1_y)
+                if p1_z <= p2_z:
+                    grid_gen(p2_x, p1_y, p1_z)
+                
+                elif p1_z > p2_z:
+                    grid_gen(p2_x, p1_y, p2_z)
                 
         return self._pts
 
 #pt1 = Point(point1[0],point1[1],point1[2])
 #pt2 = Point(point2[0],point2[1],point2[2])
-pt1 = Point(-214.272358, -47.384897, 0)
+
+pt1 = Point(-214.272358, -47.384897, 150.54862)
 pt2 = Point(126.804278, 395.601923, 0)
 matrix = Matrix(pt1,pt2,count)
 matrix = matrix.grid()
