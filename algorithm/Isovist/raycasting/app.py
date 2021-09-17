cf677a31c4abd3935d82c3f8c5e6c7ba2a3b1eee
PCH <qhqhahah@naver.com>
2021-09-17 09:18:22 +0900

    Raycsting In Progress

--- algorithm/Isovist/raycasting/app.py
+++ algorithm/Isovist/raycasting/app.py
@@ -1,19 +1,18 @@
 import numpy as np
 import json
 import pygame
-from src.ray import Limits
+import numpy as np
+from src.ray import Light, Raycasting, Wall
+
 
 class Application:
-    
     def load_json(self, file_path):
         with open(file_path, 'r') as c:
             return json.load(c)
 
-
     def __init__(self):
         pygame.init()
-
-        # app configuration
+        self.fps = 60
         self.running = True
         self.conf = self.load_json('conf.json')
         self.width = self.conf['general']['width']
@@ -22,41 +21,51 @@ class Application:
         self.screen_color = self.conf['general']['color']
         self.size = (self.width, self.height)
         self.screen = pygame.display.set_mode(self.size)
+        self.clock = pygame.time.Clock()
         pygame.display.set_caption(self.title)
 
-        # obstacles configuration
+        self.walls = []
         self.wall_color = self.conf['walls']['color']
         self.wall_width = self.conf['walls']['width']
         self.wall_coord = self.conf['walls']['walls']
 
+        self.light = Light(self.conf['ray'])
 
-        
     def draw_wall(self):
         walls = []
         for i in range(len(self.wall_coord)):
             walls.extend([self.wall_coord[i][0], self.wall_coord[i][1]])
 
         for i in range(1, len(walls), 2):
-            wall = Limits(self.wall_color, walls[i-1], walls[i], self.wall_width)
+            wall = Wall(self.wall_color, walls[i-1], walls[i], self.wall_width)
+            self.walls.append(wall)
             wall.display(self.screen)
+
+
+    def draw_light(self):
+        cursor_position = pygame.mouse.get_pos()
+        cursor_x, cursor_y = cursor_position
+        cursor_rad = 3
+        self.light.position[0] = cursor_x
+        self.light.position[1] = cursor_y
+        pygame.draw.circle(self.screen, self.light.color, cursor_position, cursor_rad)
 
 
     def run(self):
         while self.running:
-            self.screen.fill(self.screen_color)
-            self.draw_wall()
-
-            pygame.display.update()
-
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
 
-
-    
-
+            self.screen.fill(self.screen_color)
+            self.draw_wall()
+            self.draw_light()
+            self.light.display(self.screen, self.walls)
+            self.clock.tick(self.fps)
+            pygame.display.update()
 
 
 if __name__ == "__main__":
-    application = Application()
-    application.run()
+    app = Application()
+    app.run()
--- algorithm/Isovist/raycasting/src/ray.py
+++ algorithm/Isovist/raycasting/src/ray.py
@@ -1,7 +1,8 @@
 import pygame
-from numpy import array
+from numpy import array, cos, sin, linalg, deg2rad
 
-class Limits:
+
+class Wall:
     def __init__(self, color, start, end, width):
         self.color = color
         self.start = start
@@ -10,3 +11,64 @@ class Limits:
 
     def display(self, screen):
         pygame.draw.line(screen, self.color, self.start, self.end, self.width)
+
+
+class Raycasting:
+    def __init__(self, x, y, radius):
+        self.position = [x, y]
+        self.direction = array([cos(radius), sin(radius)])
+
+    # def display(self, screen):
+    #     pygame.draw.line(screen, (255,255,255), self.position, self.position+self.direction, 1)
+
+    def cast(self, wall):
+        x1 = wall.start[0]
+        y1 = wall.start[1]
+        x2 = wall.end[0]
+        y2 = wall.end[1]
+        x3 = self.position[0]
+        y3 = self.position[1]
+        x4 = self.position[0] + self.direction[0]
+        y4 = self.position[1] + self.direction[1]
+        denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+        numerator = (x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)
+
+        if denominator == 0:
+            return None  
+
+        t = numerator / denominator
+        u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / denominator
+
+        if (t > 0) and (t < 1) and (u > 0):
+            x = x1 + t * (x2 - x1)
+            y = y1 + t * (y2 - y1)
+            coordenates = array([x,y])
+            return coordenates
+
+
+class Light:
+    def __init__(self, conf):
+        self.position = array([0, 0])
+        self.color = conf['color']
+        self.width = conf['width']
+
+    def display(self, screen, walls):
+        self.rays = []
+        for i in range(0, 360, 5):
+            raycasting = Raycasting(self.position[0], self.position[1], deg2rad(i))
+            self.rays.append(raycasting)
+
+        for ray in self.rays:
+            closest = 5000
+            closestpt = None
+
+            for wall in walls:
+                casted = ray.cast(wall)
+                if casted is not None:
+                    distance = linalg.norm(casted - self.position)
+                    if distance < closest:
+                        closest = distance
+                        closestpt = casted
+
+            if closestpt is not None:
+                pygame.draw.line(screen, self.color, self.position, array(closestpt, int), self.width)
