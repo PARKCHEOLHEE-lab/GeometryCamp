e6a23b0e34cc57411fb159a2f2ba915f9ec20d96
PCH <qhqhahah@naver.com>
2021-09-27 14:17:10 +0900

    Grasshopper Geometry 201

--- exercise/p201/p201.py
+++ exercise/p201/p201.py
@@ -11,9 +11,9 @@ class Triangrid:
         self.y_count = y_count
         self.curves = self.grid_gen()
         
-    def get_guid(self, curve):
-        guid = rs.MoveObject(curve, [0,0,0])
-        return guid
+#    def get_guid(self, curve):
+#        guid = rs.MoveObject(curve, [0,0,0])
+#        return guid
         
     def grid_gen(self):
         grid = gh.Triangular(self.plane, self.size, self.x_count, self.y_count)[0]
@@ -31,9 +31,13 @@ class Triangrid:
         scaled = gh.Scale(curve, origin, factor)[0]
         return scaled
         
-    def grid_segment(self, curve, centroid):
-        segmented = gh.Loft([curve, centroid])
+    def grid_segment(self, curve, scaled):
+        segmented = rs.AddLoftSrf([curve, scaled])
         return segmented
+        
+    def grid_deconstruct(self, curve):
+        curve_list = gh.DeconstructBrep(curve)[1]
+        return curve_list  
         
     def surface_gen(self, curve):
         surface = gh.BoundarySurfaces(curve)
@@ -53,24 +57,18 @@ class Triangrid:
 
 if __name__ == '__main__':
     SCALE = 0.0001
-    plane = rg.Plane.WorldXY
-    triangles = Triangrid(plane, 3, 13, 8)
+    PLANE = rg.Plane.WorldXY
+    triangles = Triangrid(PLANE, 3, 13, 8)
     point = triangles.surface_eval(slider[0], slider[1])
     
-    grid_srfs = []
-    grid_pts = []
     grid_scaled = []
     grid_segmented = []
     for t_curve in triangles.curves:
-        triangle = triangles.surface_gen(t_curve)
         centroid = triangles.grid_centroid(t_curve)
         scaled = triangles.grid_scaled(t_curve, centroid, SCALE)
         
-        triangle_guid = triangles.get_guid(t_curve)
-        scaled_guid = triangles.get_guid(scaled)
-        segmented = triangles.grid_segment(triangle, scaled)
-        
-        grid_srfs.append(triangle)
-        grid_pts.append(centroid)
-        grid_scaled.append(scaled)
-        grid_segmented.append(segmented)
+        triangle_line_list = triangles.grid_deconstruct(t_curve)
+        scaled_line_list = triangles.grid_deconstruct(scaled)
+        for i in range(len(scaled_line_list)):
+            segmented = triangles.grid_segment(triangle_line_list[i], scaled_line_list[i])[0]
+            grid_segmented.append(segmented)
