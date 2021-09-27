42eb70a7412e8f5d8cc005628fddf4a33ba3725a
PCH <qhqhahah@naver.com>
2021-09-27 10:17:39 +0900

    Grasshopper Geometry 201

--- exercise/p201/p201.py
+++ exercise/p201/p201.py
@@ -10,23 +10,27 @@ class Triangrid:
         self.x_count = x_count
         self.y_count = y_count
         self.curves = self.grid_gen()
-
+        
     def grid_gen(self):
         grid = gh.Triangular(self.plane, self.size, self.x_count, self.y_count)[0]
         return grid
-
+        
     def grid_merge(self):
         union = gh.RegionUnion(self.curves)
         return union
-
+        
     def grid_centroid(self, curve):
-        centroid = rs.CurveAreaCentroid(curve)
+        centroid = rs.CurveAreaCentroid(curve)[0]
         return centroid
-    
+        
+    def grid_scaled(self, curve, origin, factor):
+        scaled = gh.Scale(curve, origin, factor)[0]
+        return scaled
+        
     def surface_gen(self, curve):
         surface = gh.BoundarySurfaces(curve)
         return surface
-
+        
     def surface_reparam(self):
         base_srf = self.surface_gen(self.grid_merge())
         base_srf = rs.coercesurface(base_srf)
@@ -34,20 +38,25 @@ class Triangrid:
         base_srf.SetDomain(0, domain)
         base_srf.SetDomain(1, domain)
         return base_srf
-
+        
     def surface_eval(self, u, v):
         point = rs.EvaluateSurface(self.surface_reparam(), u, v)
         return point
 
 
 if __name__ == '__main__':
+    FACTOR = 0.0001
     plane = rg.Plane.WorldXY
     triangles = Triangrid(plane, 3, 13, 8)
     point = triangles.surface_eval(slider[0], slider[1])
+    
     grid_srfs = []
     grid_pts = []
+    grid_scaled = []
     for i in triangles.curves:
-        tri = triangles.surface_gen(i)
-        cen = triangles.grid_centroid(i)[0]
-        grid_srfs.append(tri)
-        grid_pts.append(cen)
+        triangle = triangles.surface_gen(i)
+        centroid = triangles.grid_centroid(i)
+        scaled = triangles.grid_scaled(triangle, centroid, FACTOR)
+        grid_srfs.append(triangle)
+        grid_pts.append(centroid)
+        grid_scaled.append(scaled)
