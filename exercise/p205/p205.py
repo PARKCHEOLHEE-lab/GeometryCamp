9bd9d561534492ea4e26e990ad4a27367a55d2d7
ch park <qhqhahah@naver.com>
2021-09-29 13:56:35 +0900

    Grasshopper Geometry 205

--- exercise/p205/p205.py
+++ exercise/p205/p205.py
@@ -9,6 +9,7 @@ class Box:
         self.x = x
         self.y = y
         self.z = z
+        self.size = self.get_size()
         self.box = self.box_gen()
         
     def get_size(self):
@@ -16,20 +17,33 @@ class Box:
         return size_dict
         
     def box_gen(self):
-        size_x = self.get_size()['x']
-        size_y = self.get_size()['y']
-        size_z = self.get_size()['z']
-        box = gh.CenterBox(self.plane, size_x, size_y, size_z)
+        box = gh.CenterBox(self.plane, self.size['x'], self.size['y'], self.size['z'])
         return box
         
     def box_array(self):
-        size_x = self.get_size()['x']
-        size_y = self.get_size()['y']
-        size_z = self.get_size()['z']
-        array = gh.BoxArray(self.box, self.box, size_x, size_y, size_z)[0]
-        return array
+        boxes = gh.BoxArray(self.box, self.box, self.size['x'], self.size['y'], self.size['z'])[0]
+        return boxes
+        
+    def box_union(self, boxes):
+        return gh.SolidUnion(boxes)
+
+
+class Sphere:
+    def __init__(self, radius):
+        self.radius = radius
+        
+    def sphere_gen(self, base):
+        sphere = gh.Sphere(base, self.radius)
+        return sphere
 
 
 if __name__ == "__main__":
     box = Box(x,y,z)
-    box = box.box_array()
+    boxes = box.box_array()
+    boxes_union = box.box_union(boxes)
+    
+    RAD = 43
+    COUNT = 6
+    SEED = 35
+    sphere_base = gh.PopulateGeometry(boxes_union, COUNT, SEED)
+    sphere = Sphere(RAD).sphere_gen(sphere_base)
