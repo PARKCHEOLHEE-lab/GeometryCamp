9d75924ea80bfd0253788cf9faa3c3ceff6d4304
ch park <qhqhahah@naver.com>
2021-11-22 12:41:46 +0900

    Subdivision Curve

---
+++ geometryFundamental/SubdivisionCurve/subdivisionCurve.py
@@ -0,0 +1,18 @@
+﻿import Rhino.Geometry as rg
+import rhinoscriptsyntax as rs
+
+quarter = 0.25
+for j in range(count):
+    pts = []
+    for i in range(len(points)-1):
+        x1, y1, z1 = ((points[i+1] - points[i]) * quarter) + points[i]
+        pt_1 = rg.Point3d(x1, y1, z1)
+        pts.append(pt_1)
+        
+        x2, y2, z2 = ((points[i+1] - points[i]) * (1 - quarter)) + points[i]
+        pt_2 = rg.Point3d(x2, y2, z2)
+        pts.append(pt_2)
+    points = pts
+
+subdiv_points = pts
+subdiv_curve = rs.AddPolyline(points)
